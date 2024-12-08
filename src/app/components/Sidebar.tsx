e9e2ca0778927097cb6b4d196b6fec35bb3e7711
Caroly <93351229+cawx@users.noreply.github.com>
2024-12-08 14:31:00 +0200

    revamp

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,38 +1,3 @@
-import { getData } from '../utils/data'
-
-interface Tool {
-  name: string
-  description: string
-  command: string
-  docs: string
-}
-
-interface DataStructure {
-  [category: string]: {
-    [subcategory: string]: Tool[]
-  }
-}
-
 export default function Sidebar() {
-  const data: DataStructure = getData()
-
-  return (
-    <aside>
-      <ul>
-        {Object.entries(data).map(([category, subcategories]) => (
-          <li key={category}>
-            <div>{category}</div>
-            <ul>
-              {Object.entries(subcategories).map(([subcategory]) => (
-                <li key={subcategory}>
-                  {subcategory}
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
-    </aside>
-  )
+  return <aside className="bg-slate-900 px-4">Sidebar</aside>;
 }
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import Sidebar from "./components/Sidebar";
-
 export default function Home() {
   return (
     <main>
-      <div>
-        <Sidebar />
-      </div>
+      <div>Hello world.</div>
     </main>
   );
 }
