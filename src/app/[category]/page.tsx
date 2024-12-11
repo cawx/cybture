c35b0978cefe9c65e5e4fdf22fd8bbde4547bcd8
Caroly <93351229+cawx@users.noreply.github.com>
2024-12-11 23:50:49 +0200

    build fix 3

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,10 +1,11 @@
+import { Metadata } from "next";
 import { getAllCategories, getCategoryBySlug } from "../data";
 import CategoryPageClient from "./CategoryClient";
 
 export async function generateStaticParams() {
   const categories = await getAllCategories();
   return Object.entries(categories).map(([slug]) => ({
-    params: { category: slug },
+    category: slug,
   }));
 }
 
@@ -16,4 +17,17 @@ export default async function CategoryPage({
   const { category } = params;
   const categoryData = await getCategoryBySlug(category);
   return <CategoryPageClient initialCategory={categoryData} />;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { category: string };
+}): Promise<Metadata> {
+  const { category } = params;
+  const categoryData = await getCategoryBySlug(category);
+
+  return {
+    title: categoryData.name,
+  };
 }
