65f22206bc8f9e8cf4f4b04387f6c6459f3e9428
Caroly <93351229+cawx@users.noreply.github.com>
2025-01-27 17:12:23 +0200

    delpoyment fix

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState } from "react";
 import { getAllCategories } from "../data";
 import Link from "next/link";
@@ -7,13 +7,17 @@ export default function Sidebar() {
   const categories = getAllCategories();
   const [searchTerm, setSearchTerm] = useState("");
 
-  const filteredCategories = Object.entries(categories).filter(([categorySlug, category]) => {
-    const categoryMatches = category.name.toLowerCase().includes(searchTerm.toLowerCase());
-    const toolsMatch = category.tools.filter(tool => 
-      tool.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    return categoryMatches || toolsMatch.length > 0;
-  });
+  const filteredCategories = Object.entries(categories).filter(
+    ([, category]) => {
+      const categoryMatches = category.name
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase());
+      const toolsMatch = category.tools.filter((tool) =>
+        tool.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      return categoryMatches || toolsMatch.length > 0;
+    }
+  );
 
   return (
     <aside className="w-[250px] shrink-0 border-r-[1px] border-white/20">
@@ -28,22 +32,30 @@ export default function Sidebar() {
         <ul className="space-y-4 mt-4">
           {filteredCategories.map(([categorySlug, category]) => (
             <li key={categorySlug}>
-              <Link href={`/${categorySlug}`} className="text-xl font-semibold text-white/95 pt-4">
+              <Link
+                href={`/${categorySlug}`}
+                className="text-xl font-semibold text-white/95 pt-4"
+              >
                 <p>{category.name}</p>
               </Link>
               <ul className="pt-3">
-                {category.tools.filter(tool => 
-                  tool.name.toLowerCase().includes(searchTerm.toLowerCase())
-                ).map((cmd) => (
-                  <li key={cmd.id} className="border-l-2 border-white/20 pl-3">
-                    <Link
-                      className="inline-block text-white/80 hover:text-white truncate"
-                      href={`/${categorySlug}#${cmd.id}`}
+                {category.tools
+                  .filter((tool) =>
+                    tool.name.toLowerCase().includes(searchTerm.toLowerCase())
+                  )
+                  .map((cmd) => (
+                    <li
+                      key={cmd.id}
+                      className="border-l-2 border-white/20 pl-3"
                     >
-                      {cmd.name}
-                    </Link>
-                  </li>
-                ))}
+                      <Link
+                        className="inline-block text-white/80 hover:text-white truncate"
+                        href={`/${categorySlug}#${cmd.id}`}
+                      >
+                        {cmd.name}
+                      </Link>
+                    </li>
+                  ))}
               </ul>
             </li>
           ))}
